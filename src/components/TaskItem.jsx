96042d8c921d09d19d8bfdb607464995ffdf28ba
ali-zunega <ali.zunega@gmail.com>
2026-05-04 17:53:55 -0300

    feat:fix UX and add animations

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,6 +1,11 @@
-const TaskItem = ({ task, toggleTask, deleteTask }) => {
+import { capitalizeFirstLetter } from "../utils/formatText";
+const TaskItem = ({ task, toggleTask, deleteTask, isDeleting }) => {
   return (
-    <div className="list-group-item p-3 d-flex justify-content-between align-items-center animate__animated animate__fadeIn">
+    <div
+      className={`list-group-item p-3 d-flex justify-content-between align-items-center shadow animate__animated animate__fadeIn task-item ${
+        isDeleting ? "deleting" : ""
+      }`}
+    >
       <div className="d-flex align-items-center gap-4">
         <i
           className={`bi ${task.completed ? "bi-check-circle-fill text-success" : "bi-circle text-secondary"} fs-5`}
@@ -8,16 +13,19 @@ const TaskItem = ({ task, toggleTask, deleteTask }) => {
           onClick={() => toggleTask(task.id)}
         ></i>
         <span
-          className={`text-capitalize fs-5 ${
-            task.completed ? "text-decoration-line-through text-muted" : ""
+          className={`fs-5 ${
+            task.completed
+              ? "completed-animation text-decoration-line-through text-muted"
+              : ""
           }`}
         >
-          {task.text}
+          {capitalizeFirstLetter(task.text)}
         </span>
       </div>
       <button
         className="btn btn-sm text-danger border-0"
         onClick={() => deleteTask(task.id)}
+        disabled={isDeleting}
       >
         <i className="bi bi-trash fs-5"></i>
       </button>
